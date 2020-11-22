0e210f8ffa8c82b03022dde45296b6133731ac80
krzs13 <krzysztofsadowski1989@gmail.com>
2020-11-22 10:54:24 +0100

    02_07

---
+++ part_1/02_threads_and_processes/07_02.cc
@@ -0,0 +1,33 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+using std::cout;
+using std::thread;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
+
+void chef_paco() {
+  cout << "Paco started and waiting for sausage to thaw..." << '\n';
+  sleep_for(seconds(3));
+  cout << "Paco is done cutting sausage." << '\n';
+}
+
+int main(int argc, char const *argv[]) {
+  cout << "Rocky requests Paco's help." << '\n';
+  thread paco(chef_paco);
+  cout << "- Is Paco joinable? [" << (paco.joinable() ? "true" : "false") << ']'
+       << '\n';
+
+  cout << "Rocky continues cooking soup." << '\n';
+  sleep_for(seconds(1));
+  cout << "- Is Paco joinable? [" << (paco.joinable() ? "true" : "false") << ']'
+       << '\n';
+
+  cout << "Rocky patiently waits for Paco to finish and join..." << '\n';
+  paco.join();
+  cout << "- Is Paco joinable? [" << (paco.joinable() ? "true" : "false") << ']'
+       << '\n';
+
+  cout << "Rocky and Paco are both done!" << '\n';
+}
