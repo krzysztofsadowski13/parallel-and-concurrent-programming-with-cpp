1b66d7685264302cdeef4695f51c7aaf58280e7a
krzs13 <krzysztofsadowski1989@gmail.com>
2020-11-22 11:17:18 +0100

    02_09 and renamed files

---
+++ part_1/02_threads_and_processes/09_02_detached_thread.cc
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+using std::cout;
+using std::thread;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
+
+void kitchen_cleaner() {
+  while (true) {
+    cout << "Paco cleaned a kitchen." << '\n';
+    sleep_for(seconds(1));
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  thread paco(kitchen_cleaner);
+  paco.detach();
+
+  for (int i = 0; i < 3; i++) {
+    cout << "Rocky is cooking..." << '\n';
+    sleep_for(milliseconds(600));
+  }
+
+  cout << "Rocky is done!" << '\n';
+}
