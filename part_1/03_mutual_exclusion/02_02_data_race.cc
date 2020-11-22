f6c2302b098dac7e616b6f4260a9ec7dcf3f3596
krzs13 <krzysztofsadowski1989@gmail.com>
2020-11-22 11:48:29 +0100

    03_02

---
+++ part_1/03_mutual_exclusion/02_02_data_race.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+
+using std::cout;
+using std::thread;
+
+size_t garlic_counter{};
+
+void shopper() {
+  for (int i = 0; i < 10000000; i++) {
+    garlic_counter++;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  thread paco(shopper);
+  thread rocky(shopper);
+
+  paco.join();
+  rocky.join();
+
+  cout << "Paco and Rocky should buy " << garlic_counter << " garlic." << '\n';
+}
